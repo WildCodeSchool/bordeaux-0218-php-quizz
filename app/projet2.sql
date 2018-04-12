230a7858871089f928caecdb45ec074af95d3f4b
David LEBLOND <dfleblond@gmail.com>
2018-04-12 14:10:12 +0200

    modification bdd du 12_04_2018

--- app/projet2.sql
+++ app/projet2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Apr 04, 2018 at 08:16 AM
+-- Generation Time: Apr 11, 2018 at 03:24 PM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -23,56 +23,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `answers`
---
-
-CREATE TABLE `answers` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(55) NOT NULL,
-  `istrue` tinyint(1) NOT NULL,
-  `id_questions` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `questions`
---
-
-CREATE TABLE `questions` (
-  `id` int(11) NOT NULL,
-  `question` varchar(255) NOT NULL,
-  `id_quizz` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `quizz`
---
-
-CREATE TABLE `quizz` (
-  `id` int(11) NOT NULL,
-  `theme` varchar(25) DEFAULT NULL,
-  `id_users` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `score`
---
-
-CREATE TABLE `score` (
-  `id_users` int(11) NOT NULL,
-  `id_quizz` int(11) NOT NULL,
-  `score` int(11) NOT NULL,
-  `date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users`
 --
 
@@ -80,7 +30,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `firstName` varchar(25) NOT NULL,
   `lastName` varchar(25) NOT NULL,
-  `adress` varchar(50) DEFAULT NULL,
+  `adress` tinytext,
   `mail` varchar(25) NOT NULL,
   `password` varchar(25) NOT NULL,
   `status` varchar(25) NOT NULL
@@ -89,34 +39,6 @@ CREATE TABLE `users` (
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `answers`
---
-ALTER TABLE `answers`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_questions` (`id_questions`);
-
---
--- Indexes for table `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_quizz` (`id_quizz`);
-
---
--- Indexes for table `quizz`
---
-ALTER TABLE `quizz`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_users` (`id_users`);
-
---
--- Indexes for table `score`
---
-ALTER TABLE `score`
-  ADD KEY `id_users` (`id_users`),
-  ADD KEY `id_quizz` (`id_quizz`);
 
 --
 -- Indexes for table `users`
@@ -129,57 +51,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `answers`
---
-ALTER TABLE `answers`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `questions`
---
-ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `quizz`
---
-ALTER TABLE `quizz`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `answers`
---
-ALTER TABLE `answers`
-  ADD CONSTRAINT `answers_ibfk_1` FOREIGN KEY (`id_questions`) REFERENCES `questions` (`id`);
-
---
--- Constraints for table `questions`
---
-ALTER TABLE `questions`
-  ADD CONSTRAINT `questions_ibfk_1` FOREIGN KEY (`id_quizz`) REFERENCES `quizz` (`id`);
-
---
--- Constraints for table `quizz`
---
-ALTER TABLE `quizz`
-  ADD CONSTRAINT `quizz_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `score`
---
-ALTER TABLE `score`
-  ADD CONSTRAINT `score_ibfk_1` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `score_ibfk_2` FOREIGN KEY (`id_quizz`) REFERENCES `quizz` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
