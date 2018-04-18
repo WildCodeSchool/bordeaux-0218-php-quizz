a72210207c0fb5447df80f3ee688d08da877329d
Marie_Carrere <marie.carrere@icloud.com>
2018-04-18 11:58:48 +0200

    updated getter & setter file users.php

---
+++ Marie_file_user/Users.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Model;
+
+/**
+ * Class User
+ *
+ */
+
+class Users
+{
+    private $id;
+
+    private $firstName;
+
+    private $lastName;
+
+    //private $address;
+
+    private $mail;
+
+    private $password;
+
+
+// Get & Setter pour l'id :
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     *
+     * @return Users
+     */
+    public function setId($id): User
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+
+// Get & Setter pour firstName :
+    /**
+     * @return mixed
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $title
+     *
+     * @return User
+     */
+    public function setFirstName($firstName):User
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+// Get & Setter pour lastName :
+    /**
+     * @return mixed
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $title
+     *
+     * @return User
+     */
+    public function setLastName($lastName):User
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+}
+
+// Get & Setter pour l'email :
+/**
+ * @return mixed
+ */
+public function getMail(): string
+  {
+    return $this->mail;
+  }
+
+/**
+ * @param mixed $title
+ *
+ * @return User
+ */
+public function setMail($mail):User
+  {
+    $this->mail = $mail;
+
+    return $this;
+  }
+}
+
+// Get & Setter pour le mot de passe :
+/**
+ * @return mixed
+ */
+public function getPassword(): string
+  {
+    return $this->password;
+  }
+
+/**
+ * @param mixed $title
+ *
+ * @return User
+ */
+public function setPassword($password):User
+  {
+    $this->password = $password;
+
+    return $this;
+  }
+}
